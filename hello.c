63bc3777a801e1239ab16993ad460a7d69400d20
AlexMinculescu <alexandru-marian.minculescu@my.fmi.unibuc.ro>
2020-04-03 10:52:11 +0300

    second commit

--- hello.c
+++ hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(){
-	printf("Hello, World!\n");
+int main(int argc, char *argv[]){
+	if(argc != 2){
+		printf("USAGE: %s name\n", argv[0]);
+	return -1;
+	}
+	printf("Hello, %s!\n", argv[1]);
 	return 0;
 }
